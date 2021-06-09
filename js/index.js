e72d2ecb62dbee593de9a717b9bf668e8f77f2d8
GalbaKor <cameron.tebbenham@outlook.com>
2021-06-10 00:53:29 +0100

    Logos and Buttons
    Toggleable work showcase buttons implemented.
    
    Logo added.
    
    Top Button function added.

--- js/index.js
+++ js/index.js
@@ -12,3 +12,38 @@ navLinks.forEach(link => {
     });
 });
 // for each link, listen for a click, then if clicked -> remove the nav-open that was previously added
+
+
+const portfolioWebsiteToggle = document.querySelector('.portfolio__website--toggle');
+const portfolioUxToggle = document.querySelector('.portfolio__ux--toggle');
+const portfolio3dToggle = document.querySelector('.portfolio__3d--toggle');
+
+portfolioWebsiteToggle.addEventListener('click', () => {
+     document.body.classList.toggle('portfolio__website--off');
+ });
+
+ portfolioUxToggle.addEventListener('click', () => {
+    document.body.classList.toggle('portfolio__ux--off');
+});
+
+portfolio3dToggle.addEventListener('click', () => {
+    document.body.classList.toggle('portfolio__3d--off');
+});
+
+
+const topButton = document.getElementById("top-btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
